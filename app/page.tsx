58c6a88f056aa7bd0a1dadf3bc857fc75fd70ecb
christopher ha <63668974+christopher-ha@users.noreply.github.com>
2023-11-03 20:07:44 -0400

    feat: Add typography and hero section.
    Initialized typography styles and completed hero section for mobile.

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,130 @@
-import Image from 'next/image'
+import Image from 'next/image';
+
+const codeBlock = `import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					{/*Fonts*/}
+					<link rel="preconnect" href="https://rsms.me" crossOrigin="anonymous" />
+					<link rel="preconnect" href="https://kit.fontawesome.com" crossOrigin="anonymous" />
+					<link rel="preload" href="https://rsms.me/inter/inter.css" as="style" />
+					<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+					<noscript>
+						<link rel="stylesheet" type="text/css" href="https://rsms.me/inter/inter.css" />
+					</noscript>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
+
+export default MyDocument
+`;
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="flex flex-col content-center justify-center">
+      <section className="flex flex-col content-center justify-center">
+        {/* Header and Description */}
+        <section className="relative z-10 m-12 text-center">
+          <h1>
+            jamstack{' '}
+            <span className="bg-gradient-to-r from-orange-300 via-red-300 to-purple-400 bg-clip-text text-transparent">
+              101
+            </span>
+          </h1>
+          <h3 className="bg-gradient-to-t from-neutral-800 to-neutral-400 bg-clip-text  pt-4 text-center text-transparent">
+            Uncover the synergy of JavaScript, APIs, and Markup to build faster,
+            more secure, and scalable websites.
+          </h3>
+        </section>
+        {/* Rectangular gradients and blur */}
+        <section className="hero">
+          <Image
+            className="hero__image"
+            src="/gradient-left.jpg"
+            width={280}
+            height={630}
+            quality={100}
+            priority
+            alt="A gradient fade between yellow and orange"
+          />
+          <Image
+            className="hero__image"
+            src="/gradient-right.jpg"
+            width={280}
+            height={670}
+            quality={100}
+            priority
+            alt="A gradient fade between yellow and orange"
+          />
+          <div className="hero__image hero__blur"></div>
+          <div className="hero__image hero__blur"></div>
+        </section>
+        {/* Pop out section with computer */}
+        <section className="hero__full z-10 flex-col content-center justify-center p-8">
+          <h1 className="text-center text-white">javascript</h1>
+          <h4 className="pb-20 pt-8 text-center text-white">
+            Statically generated pages are brought to life with client-side
+            JavaScript libraries and frameworks, such as Algolia and Next.js.
+          </h4>
+          {/* Render codeBlock as string*/}
+          <div className="relative">
+            <pre className="absolute z-20">
+              <code className="hero__code text font-mono text-[0.25rem] text-cyan-300">
+                {codeBlock}
+              </code>
+            </pre>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/computer.png"
+              className="scale-125"
+              width={360}
+              height={360}
+              alt="A vector rendering of a CRT computer."
             />
-          </a>
+          </div>
+        </section>
+      </section>
+      {/* Logos */}
+      <section className="z-0 m-0 flex h-[1200px] w-screen -translate-y-96 justify-center bg-neutral-900 pt-96">
+        <div className="flex flex-col items-center pt-16 text-center">
+          <Image
+            className="logo"
+            alt="React.js Logo"
+            src="/react.svg"
+            width={140}
+            height={64}
+          />
+          <Image
+            className="logo"
+            alt="Next.js Logo"
+            src="/next.svg"
+            width={128}
+            height={64}
+          />
+          <Image
+            className="logo"
+            alt="Vue.js Logo"
+            src="/vue.svg"
+            width={140}
+            height={64}
+          />
+          <Image
+            className="logo"
+            alt="Svelte.js Logo"
+            src="/svelte.svg"
+            width={150}
+            height={64}
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
-  )
+  );
 }
