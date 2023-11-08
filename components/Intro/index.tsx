a409b73b680164277f8b5082bc0f840a39ee6059
christopher ha <63668974+christopher-ha@users.noreply.github.com>
2023-11-07 19:57:24 -0500

    feat: Add rotation on scroll for mouse trail
    Using same logic, we can manipulate mouse rotation and origin on scroll to mimic it moving downwards leading the eye to the next hero.

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -17,6 +17,13 @@ export default function Intro() {
 
   const y1 = useTransform(scrollY, (value) => value * -0.1 + 100);
   const y2 = useTransform(scrollY, (value) => value * -0.2 + 100);
+  const yh = useTransform(scrollY, (value) => value * -0.3);
+  const yh2 = useTransform(scrollY, (value) => value * -0.25);
+  const r = useTransform(scrollY, [0, 1200], [0, 60]);
+
+  useMotionValueEvent(r, 'change', (latest) => {
+    console.log(latest);
+  });
 
   return (
     <section
@@ -25,26 +32,34 @@ export default function Intro() {
     >
       {/* Header and Description */}
       <section className="relative z-10 m-12 flex flex-col items-center justify-center text-center md:m-16 md:mb-32 lg:z-10 lg:m-24 lg:items-start lg:justify-start lg:text-left">
-        <h1>
+        <m.h1 style={{ y: yh }}>
           jamstack{' '}
           <span className="bg-gradient-to-r from-orange-300 via-red-300 to-purple-400 bg-clip-text text-transparent">
             101
           </span>
-        </h1>
-        <h3 className="bg-gradient-to-t from-neutral-800 to-neutral-400 bg-clip-text pt-4 text-center text-transparent md:w-10/12 lg:w-5/12 lg:text-left">
+        </m.h1>
+        <m.h3
+          style={{ y: yh2 }}
+          className="bg-gradient-to-t from-neutral-800 to-neutral-400 bg-clip-text pt-4 text-center text-transparent md:w-10/12 lg:w-5/12 lg:text-left"
+        >
           Uncover the synergy of JavaScript, APIs, and Markup to build faster,
           more secure, and scalable websites.
-        </h3>
+        </m.h3>
       </section>
-      <Image
-        className="mouse"
-        src="/mouse.png"
-        width={285}
-        height={705}
-        quality={100}
-        priority
-        alt="A computer mouse trailing a dotted line"
-      />
+      <m.div
+        className="md: absolute -bottom-[5rem] left-[3rem] origin-[-150px] md:-bottom-[12rem] md:left-[8rem] md:origin-[-400px] lg:-bottom-[32rem] lg:left-64 lg:origin-[-450px]"
+        style={{ rotate: r }}
+      >
+        <Image
+          className="w-[200px] md:w-[300px] lg:w-[350px]"
+          src="/mouse.png"
+          width={285}
+          height={705}
+          quality={100}
+          priority
+          alt="A computer mouse trailing a dotted line"
+        />
+      </m.div>
       {/* Rectangular gradients and blur + Javascript */}
       <section className="hero h-[700px] md:h-[1200px] lg:h-[1400px]">
         <m.div style={{ y: y1 }}>
