b3425087c16347ba824c876ad5c3993231e6fa3f
christopher ha <63668974+christopher-ha@users.noreply.github.com>
2023-11-08 17:19:31 -0500

    fix: Add back in Lenis Scroll

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,14 @@ import { useEffect } from 'react';
 import Lenis from '@studio-freight/lenis';
 
 export default function Home() {
-  // useEffect(() => {
-  //   const lenis = new Lenis();
-  //   function raf(time: number) {
-  //     lenis.raf(time);
-  //     requestAnimationFrame(raf);
-  //   }
-  //   requestAnimationFrame(raf);
-  // }, []);
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
 
   return (
     <LazyMotion features={domAnimation}>
